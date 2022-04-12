c93b3412dc989efa476eabed0ec1375277e3f50c
azharuddinkhan <azhar.sqa@hotmail.com>
2022-04-12 15:40:11 +0530

    12th Commit

--- src/test/java/tests/ReltioPOCTest.java
+++ src/test/java/tests/ReltioPOCTest.java
@@ -43,6 +43,24 @@ public class ReltioPOCTest extends TestBase {
         page.ReltioProfilePage().verifybfoUserCreation(bfoId,accountName, data.get("ClassificationLevel"), data.get("MarketSegment"), data.get("SubMarketSegment"));*/
 	}
 
-
+	@Test(priority = 2,dataProvider="ExcelDP1",dataProviderClass=TestUtil.class)
+	public void reltioPOC1(Hashtable<String, String> data) throws Exception {
+		page.BasePage().navigateToApplication("http://www.google.com");
+		ExtentTestManager.getTest().log(LogStatus.INFO, "Navigating to Google.com");
+		ExtentTestManager.getTest().log(LogStatus.INFO, "Statement 1");
+		ExtentTestManager.getTest().log(LogStatus.INFO, "Statement 1");
+		ExtentTestManager.getTest().log(LogStatus.INFO, "Statement 1");
+		/*page.loginPage().LogintoApplication(data.get("bfoUser"), data.get("bfoPassword"));
+		page.AccountsPage().createAccount(data.get("AcountName"), data.get("Address"), data.get("SelectAddress"),
+				data.get("ClassificationLevel"), data.get("MarketSegment"), data.get("SubMarketSegment"));
+		page.AccountsPage().clickOnAccountsLink();
+		String accountName = page.AccountsPage().AccountCreation();
+		String bfoId = page.AccountsPage().getbfoID();
+		
+        page.AccountsPage().openNewTab();
+        page.BasePage().navigateToApplication(config.getProperty("Reltio_URL", CONFIG.APPLICATION));
+        page.loginPage().LogintoReltioApplication(data.get("CDLUser"), data.get("CDLPassword"));
+        page.ReltioProfilePage().verifybfoUserCreation(bfoId,accountName, data.get("ClassificationLevel"), data.get("MarketSegment"), data.get("SubMarketSegment"));*/
+	}
 
 }
