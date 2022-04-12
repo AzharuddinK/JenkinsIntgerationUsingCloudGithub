1896a875bd83015c874f5b2df0ba5076a31ad139
azharuddinkhan <azhar.sqa@hotmail.com>
2022-04-12 20:37:07 +0530

    new Jar to write to create JSON file for latest reportname with full path

--- src/test/java/tests/GetLatestReportPath.java
+++ src/test/java/tests/GetLatestReportPath.java
@@ -1,11 +1,17 @@
 package tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import utility.FileUtility;
+import utility.JSONUtils;
 
 public class GetLatestReportPath {
-	
+	static Map<String,String> data_for_Json = new HashMap<>();
 	public static void main(String[] args) {
-		System.out.println(FileUtility.getLastModifiedFileName(System.getProperty("user.dir")+"/ExtentReports/"));
+		String FileName = FileUtility.getLastModifiedFileName(System.getProperty("user.dir")+"/ExtentReports/");
+		data_for_Json.put("ReportName", FileName);
+		JSONUtils.writeObjects2JsonFile(data_for_Json, System.getProperty("user.dir")+"/ExtentReports/TestReport.json");
 	}
 
 }
